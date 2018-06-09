ba3ba6aa05497f2a4ee3e8385b215ec996d86e75
Ronak <ronakkatta28@gmail.com>
2018-06-09 15:56:38 +0530

    trial change

---
+++ app/src/main/java/com/example/user/musicapp/SampleMusic.java
@@ -0,0 +1,15 @@
+package com.example.user.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_music);
+
+//do
+    }
+}
