085ece30397e7d9f78b06b7542baddc039889586
Ronak <ronakkatta28@gmail.com>
2018-06-09 16:04:50 +0530

    trial change

--- app/src/main/java/com/example/user/musicapp/SampleMusic.java
+++ app/src/main/java/com/example/user/musicapp/SampleMusic.java
@@ -1,15 +1,23 @@
 package com.example.user.musicapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SampleMusic extends AppCompatActivity {
-
+public MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_music);
 
-//do
+
+    }
+
+    public void playSample(View view)
+    {
+       mp=MediaPlayer.create(this,R.raw.sample);
+       mp.start();
     }
 }
