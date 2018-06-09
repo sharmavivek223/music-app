93cec374b2adfe47fa85a15e0a6510d213e4a408
Ronak <ronakkatta28@gmail.com>
2018-06-09 18:41:13 +0530

    SampleMusicPlayer

--- app/src/main/java/com/example/user/musicapp/MainActivity.java
+++ app/src/main/java/com/example/user/musicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
+    }
+    public void sampleActivity(View view)
+    {
+        Intent i=new Intent(this,SampleMusic.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/user/musicapp/SampleMusic.java
+++ app/src/main/java/com/example/user/musicapp/SampleMusic.java
@@ -1,23 +1,101 @@
 package com.example.user.musicapp;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.RemoteController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class SampleMusic extends AppCompatActivity {
-public MediaPlayer mp;
+private MediaPlayer mp;
+private AudioManager mAudioManager;
+
+    AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                        // Pause playback because your Audio Focus was
+                        // temporarily stolen, but will be back soon.
+                        // i.e. for a phone call
+                        mp.pause();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // Stop playback, because you lost the Audio Focus.
+                        // i.e. the user started some other playback app
+                        // Remember to unregister your controls/buttons here.
+                        // And release the kra — Audio Focus!
+                        // You’re done.
+                         releaseMediaPlayer();
+                    } else if (focusChange ==
+                            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Lower the volume, because something else is also
+                        // playing audio over you.
+                        // i.e. for notifications or navigation directions
+                        // Depending on your audio playback, you may prefer to
+                        // pause playback here instead. You do you.
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Resume playback, because you hold the Audio Focus
+                        // again!
+                        // i.e. the phone call ended or the nav directions
+                        // are finished
+                        // If you implement ducking and lower the volume, be
+                        // sure to return it to normal here, as well.
+                        mp.start();
+                    }
+                }
+            };
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_music);
 
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
 
     }
+//AudioManager.OnAudioFocusChangeListener();
 
     public void playSample(View view)
-    {
-       mp=MediaPlayer.create(this,R.raw.sample);
-       mp.start();
+    {  releaseMediaPlayer();
+
+        int result=mAudioManager.requestAudioFocus(afChangeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN);
+        if(result==AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+        {
+          //  mAudioManager.registerMediaButtonEventReceiver(RemoteController);
+            mp=MediaPlayer.create(this,R.raw.sample);
+            mp.start();
+
+        }
+
+
+
+
+
     }
+    //Clean up the media player by releasing its resources.
+
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mp != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mp.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mp= null;
+            mAudioManager.abandonAudioFocus(afChangeListener);
+        }
+    }
+
+
 }
